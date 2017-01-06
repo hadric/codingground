7adb15b7928eb7bca0b850856eeff3cc38b52313
hadric <hardiksomani90@yahoo.com>
2017-01-06 12:47:36 +0530

    Created by Coding Ground Client

---
+++ New Project-20170106/DuplicateKeys.java
@@ -0,0 +1,17 @@
+class DuplicateKeys{
+public static void main(String[] Args)
+    {
+        Scanner sc = new Scanner(System.in);
+        //int i=0,dec=0;
+        System.out.println("enter a binary number:");
+        int x = sc.nextInt();
+        /*While( x!=0 ){
+            int y = x%10;
+            x = x/10;
+            dec = dec + (y*(2^i));
+            i++;
+        }*/
+        /*System.out.println("decimal number:");
+        System.out.println(dec);*/
+    }
+}
